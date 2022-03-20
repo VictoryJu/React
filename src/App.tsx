859e7463d685861db9a8c62b8db215df0beede7a
VictoryJu <kob0805@naver.com>
2022-03-20 22:34:36 +0900

    React UseState

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  let [title,setTitle] = useState(['리스트1','리스트2','리스트3'])
+  let [date,setDate] = useState('날짜')
+  let [count,setCount] = useState(0);
+
+  function editTitle(){
+    let newArray = [...title];
+    newArray[0] = "여자코트 추천";
+    setTitle(newArray);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav-bar">리액트 튜토리얼</div>
+      <div className="main-container">
+        <div className='main-wrap'>
+          <button onClick={editTitle}>제목바꿈 버튼</button>
+          <div className="title">{title[0]} <span onClick={()=>setCount(count+=1)}>👍</span> {count} </div>
+          <div>{date}</div>
+        </div>
+        <div className='main-wrap'>
+          <div className="title">{title[1]}</div>
+          <div>{date}</div>
+        </div>
+        <div className='main-wrap'>
+          <div className="title">{title[2]}</div>
+          <div>{date}</div>
+        </div>
+      </div>
     </div>
   );
 }
